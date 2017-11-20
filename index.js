5b548f22d6f543fb9b2c9d313b4f561226d8c735
Gonzodamus <Dick@TheDickWard.com>
2017-11-20 17:40:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file
+function findMatching (array, string) {
+  return array.filter(function (word) { return word.toLowerCase() === string.toLowerCase() })
+}
+
+function fuzzyMatch (array, string) {
+  return array.filter(function (word) {
+    return word.slice(0, string.length) === string
+  })
+}
+
+function matchName (array, string) {
+  return array.filter(function (word) { return word['name'] === string })
+}
