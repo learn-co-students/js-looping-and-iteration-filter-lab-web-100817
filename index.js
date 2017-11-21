3a64cb99101228e512d8f2a6ce16b0f6f2e6b35a
flored27 <dflores@fandm.edu>
2017-11-21 09:31:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(list, name) {
+
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase()
+  });
+}
+
+function fuzzyMatch (list, name) {
+  return list.filter(function (driverName){
+    return driverName.charAt(0) === name.charAt(0)
+  });
+}
+
+function matchName (list, nombre) {
+  return list.filter(function (driverName) {
+    return driverName.name.toLowerCase() === nombre.toLowerCase()
+  });
+}
