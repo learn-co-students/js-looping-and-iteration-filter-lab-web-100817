c1066ba0b0266869e908980b14c0bb5cac8fe72e
Fabiano Cunha <cunhasb@aol.com>
2017-11-23 22:27:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+
+function findMatching(array,name){
+// return array.filter(function(element) {
+//     return element.toLowerCase() === name.toLowerCase();
+// });
+// or
+  return array.filter(element => element.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(array,string){
+  return array.filter(function(element){
+    return element.toLowerCase().slice(0,2).includes(string.toLowerCase())})
+}
+
+function matchName(array,name){
+  return array.filter(function(element){
+    return element.name.toLowerCase() === name.toLowerCase()
+  })
+}
